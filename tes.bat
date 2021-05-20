e7a6c7c2e8fddf953a57dbc3e36a63e17fe80596
reynaldi <reynaldi@bounche.com>
2021-05-20 15:33:36 +0700

    init

---
+++ tes.bat
@@ -0,0 +1,5 @@
+python main.py -i .\docs\imgs\tes\ -o .\docs\imgs\output\tes\u2net\ -m u2net
+python main.py -i .\docs\imgs\tes\ -o .\docs\imgs\output\tes\basnet\ -m basnet
+python main.py -i .\docs\imgs\tes\ -o .\docs\imgs\output\tes\u2netp\ -m u2netp
+python main.py -i .\docs\imgs\tes\ -o .\docs\imgs\output\tes\xception_model\ -m xception_model
+python main.py -i .\docs\imgs\tes\ -o .\docs\imgs\output\tes\mobile_net_model\ -m mobile_net_model
